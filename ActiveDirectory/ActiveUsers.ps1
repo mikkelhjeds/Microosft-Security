4ab3cc12ad8aa65e9e22cb94e425909b5b6bf092
Mikkel Hjeds <mkklhjeds@gmail.com>
2023-03-16 16:05:03 +0100

    Added.

--- ActiveDirectory/ActiveUsers.ps1
+++ ActiveDirectory/ActiveUsers.ps1
@@ -6,7 +6,7 @@ Import-Module ActiveDirectory
 
 $inactiveusers = @()
 
-$blockedusers = Get-AdUser -filter * -Properties * | select UserPrincipalName, Enabled, WhenCreated, LastLogonDate, CreateTimeStamp, ModifyTimeStamp
+$blockedusers = Get-AdUser -filter * -Properties * | select UserPrincipalName, SamAccountName, Enabled, WhenCreated, LastLogonDate, CreateTimeStamp, ModifyTimeStamp
 $currentdate = Get-Date
 
 foreach ($user in $blockedusers) {
@@ -14,14 +14,26 @@ foreach ($user in $blockedusers) {
     if ($user.WhenCreated) {
         $diffwhencreated = (New-TimeSpan -Start $user.WhenCreated -End $currentdate)
     }
+    else {
+        $diffwhencreated = "-"
+    }
     if ($user.LastLogonDate) {
         $difflastlogondate = (New-TimeSpan -Start $user.LastLogonDate -End $currentdate)
+    }
+    else {
+        $difflastlogondate = "-"
     }
     if ($user.CreateTimeStamp) {
         $diffcreatetimestamp = (New-TimeSpan -Start $user.CreateTimeStamp -End $currentdate)
     }
+    else {
+        $diffcreatetimestamp = "-"
+    }
     if ($user.ModifyTimeStamp) {
         $diffmodifytimestamp = (New-TimeSpan -Start $user.ModifyTimeStamp -End $currentdate)
+    }
+    else {
+        $diffmodifytimestamp = "-"
     }
 
     # $diffwhencreated = (New-TimeSpan -Start $user.WhenCreated -End $currentdate)
@@ -30,7 +42,8 @@ foreach ($user in $blockedusers) {
     # $diffmodifytimestamp = (New-TimeSpan -Start $user.ModifyTimeStamp -End $currentdate)
 
     $inactiveusers += [PSCustomObject]@{
-        email = $user.UserPrincipalName
+        upn = $user.UserPrincipalName
+        sam = $user.SamAccountName
         userenabled = $user.Enabled
         whencreated = $user.WhenCreated
         lastlogondate = $user.LastLogonDate
@@ -43,4 +56,4 @@ foreach ($user in $blockedusers) {
 
 }
 
-$inactiveusers | Export-Csv -Path ("C:\Temp\ActiveUsers" + (Get-Date -Format "dddd/MM/ss/yyyy-HH_mm") + ".csv") -Delimiter ";" -NoTypeInformation
+$inactiveusers | Export-Csv -Path ("C:\Temp\ActiveUsers-" + (Get-Date -Format "dd-MM-yyyy-HH_mm") + ".csv") -Delimiter ";" -NoTypeInformation
