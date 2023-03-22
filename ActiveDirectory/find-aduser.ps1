0c4f252ab00820a8759d87b31de7d4a4c1efea90
Mikkel Hjeds <mkklhjeds@gmail.com>
2023-03-22 13:17:54 +0100

    Different Structure.

--- ActiveDirectory/find-aduser.ps1
+++ ActiveDirectory/find-aduser.ps1
@@ -10,52 +10,29 @@ param (
 
 Import-Module ActiveDirectory
 
-$domainkoncern = "koncern.local"
-$domainfoodit = "foodit.local"
-$domainbutik = "butik.local"
+$domains = @("koncern.local", "foodit.local", "butik.local")
 
 function CheckDomains {
-
+	
 	try {
-		write "Checking $user in $domainkoncern."
-		$koncerndata = Get-ADUser -Server $domainkoncern -Identity $User | select Name, UserPrincipalName, Enabled
+		$user = Get-ADUser -Server $domain -Identity $User
 	}
 	catch {
-		write "Could not find user """$user""" in $domainkoncern."
-	}
-
-	try {
-		write "Checking $user in $domainfoodit"
-		$fooditdata = Get-ADUser -Server $domainfoodit -Identity $User | select Name, UserPrincipalName, Enabled
-	}
-	catch {
-		write "Could not find user """$user""" in $domainfoodit."
-	}
-
-	try {
-		write "Checking $user in $domainbutik"
-		$butikdata = Get-ADUser -Server $domainbutik -Identity $User | select Name, UserPrincipalName, Enabled
-	}
-	catch {
-		write "Could not find user """$user""" in $domainbutik."
 	}
 	
+
 	$returnuser = [PSCustomObject]@{
-		koncernName = $koncerndata.Name, $fooditdata.Name, $butikdata.Name
-		koncernUpn = $koncerndata.UserPrincipalName, $fooditdata.UserPrincipalName, $butikdata.UserPrincipalName
-		koncernEnabled = $koncerndata.Enabled, $fooditdata.Enabled, $butikdata.Name
-		# fooditName = $fooditdata.Name
-		# fooditUpn = $fooditdata.UserPrincipalName
-		# fooditEnabled = $fooditdata.Enabled
-		# butikName = $butikdata.Name
-		# butikUpn = $butikdata.UserPrincipalName
-		# butikEnabled =  $butikdata.Name
+		Domain  = $Domain
+		Name    = $user.Name
+		UPN     = $user.UserPrincipalName
+		Enabled = $user.Enabled
 	}
 
 	return $returnuser
 }
 
-CheckDomains | ft
+foreach ($domain in $domains) {
+	
+	CheckDomains
 
-
-
+}
